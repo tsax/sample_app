bb83b3debdec286a6dc6eec8f00e3696bae880bc
Tushar S <tsaxena86@gmail.com>
2014-09-12 01:00:22 -0400

    Heroku please work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ SampleApp::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.assets.raise_production_errors = true
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
